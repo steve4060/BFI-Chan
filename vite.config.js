b3b3acefa095447eb50b6bfe3c62908669f5d719
steve4060 <steve.4060.ri@gmail.com>
2025-05-11 14:57:32 +0200

    added Json and Router

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import jsonServer from 'vite-plugin-simple-json-server';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), jsonServer({
+        urlPrefixes: [
+            '/json/'
+        ],
+        limit: 100,
+        mockDir: 'json',
+        staticDir: 'public',
+    })],
 })
