906a631064d03e6170a2615febdf191781edd767
steve4060 <92173331+steve4060@users.noreply.github.com>
2025-05-11 13:05:04 +0000

    Added Landingpage

---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import './LandingPage.css';
+
+function LandingPage() {
+    return (
+        <>
+            <div>
+                <h1>Landing Page</h1>
+            </div>
+        </>
+    )
+}
+
+export default LandingPage;
