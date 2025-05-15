7cfa6c1f5a8531feb6d743d56ade3ba6dd682ca9
generalbeni <156806863+generalbeni@users.noreply.github.com>
2025-05-15 06:46:34 +0000

    Added Boardcard.jsx

---
+++ src/components/Boardcard/Boardcard.jsx
@@ -0,0 +1,14 @@
+function Boardcard(){
+  return (
+    <div className="col">
+      <div className="card h-100">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Board 1</h5>
+          <p className="card-text">This is a short card.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Boardcard;
